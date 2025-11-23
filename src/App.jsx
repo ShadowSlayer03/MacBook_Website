bd95e2b6c89155efd0432b67218360a9f757891a
ShadowSlayer03 <sslayer441@gmail.com>
2025-11-23 17:19:01 +0530

    Added product viewer section, highlights and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import ProductViewer from './components/ProductViewer';
+
+import gsap from 'gsap';
+import { ScrollTrigger, SplitText } from 'gsap/all';
+import Highlights from './components/Highlights';
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
     <main>
         <Navbar />
         <Hero />
+        <ProductViewer />
+
+        <Highlights />
     </main>
   )
 }
